7eea3c3a1e2a41ce741ded24fae8095dd64a89c3
romancehl2 <heziyuanosu@hotmail.com>
2020-08-22 21:12:02 +0800

    11

---
+++ e374/e374.cpp
@@ -0,0 +1,42 @@
+﻿/****************************************************************************
+**
+** 编写一个程序，要求用户以整数方式输入秒数（使用long或long long变量存储），然后以天
+** 、小时、分钟和秒的方式显示这段时间。使用符号常量来表示每天有多少小时、每小时有多少
+** 分钟一集每分钟有多少秒。该程序的输出应与下面类似。
+** Enter the number of seconds: 31600000
+** 31600000 seconds = 365 days, 17 hours, 46 minutes, 40 seconds.
+** 
+****************************************************************************/
+
+#include <iostream>
+
+const int hours_per_day = 24;
+const int minutes_per_hour = 60;
+const int seconds_per_minute = 60;
+
+int main()
+{
+    long second_sum = 0;
+    int days = 0;
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+
+    using namespace std;
+
+    cout << "Enter the number of seconds: ";
+    cin >> second_sum;
+
+    seconds = second_sum % seconds_per_minute;
+    minutes = second_sum / seconds_per_minute % minutes_per_hour;
+    hours   = second_sum / seconds_per_minute / minutes_per_hour % hours_per_day;
+    days    = second_sum / seconds_per_minute / minutes_per_hour / hours_per_day;
+
+    cout << second_sum << " seconds = "   \
+        << days << " days, "              \
+        << hours << " hours, "            \
+        << minutes << " minutes, "        \
+        << seconds << " seconds.";
+
+    return 0;
+}
---
+++ e375/e375.cpp
@@ -0,0 +1,32 @@
+﻿/****************************************************************************
+**
+** 编写一个程序，要求用户输入全球当前的人口和美国的人口（或其他国家的人口）。将这些信
+** 存储在long long变量中，并让程序显示美国（或其他国家）的人口占全球人口的百分比。该
+** 程序的输出应与下面类似：
+** Enter the world's population: 689875899
+** Enter the ppulation of the US: 310783781
+** The population of the US is 4.50492% of the world population.
+**
+****************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    long long population_sum = 0;
+    long long population_us = 0;
+    double rate = 0;
+
+    using namespace std;
+
+    cout << "Enter the world's population: ";
+    cin >> population_sum;
+    cout << "Enter the ppulation of the US: ";
+    cin >> population_us;
+
+    rate = (double)population_us / population_sum * 100;
+
+    cout << "The population of the US is "<< rate <<"% of the world population.";
+
+    return 0;
+}
