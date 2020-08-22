ece7749cfee45b47153205646104e43bb8843e38
romancehl2 <heziyuanosu@hotmail.com>
2020-08-22 17:27:07 +0800

    add e372.cpp

--- e372/e372.cpp
+++ e372/e372.cpp
@@ -11,31 +11,52 @@
 
 #include <iostream>
 
+int myFunction1(int height_in_foot, int height_in_less_inch);
+double myFunction2(double weight_in_pound, int height_in_inch);
+
 const int factor_foot_to_inch = 12;
-const float factor_inch_to_meter = 0.0254;
+const double factor_inch_to_meter = 0.0254;
+const double factor_km_to_pound = 2.2;
 
 int main()
 {
     int height_in_foot;
     int height_in_less_inch;
-    float weight_in_pound;
-    float weight_in_km;
+    double weight_in_pound;
+    int height_in_inch;
 
-    std::cout << "Enter your height in foot and inch: " << std::endl;
-    std::cout << "foot: ";
-    std::cin >> height_in_foot;
-    std::cout << "inch: ";
-    std::cin >> height_in_less_inch;
-    std::cout << "Enter your weight in pound: " << std::endl;
-    std::cin >> weight_in_pound;
+    using namespace std;
 
+    cout << "Enter your height in foot and inch: " << endl;
+    cout << "foot: ";
+    cin >> height_in_foot;
+    cout << "inch: ";
+    cin >> height_in_less_inch;
+    cout << "Enter your weight in pound: " << endl;
+    cin >> weight_in_pound;
 
+    height_in_inch = myFunction1(height_in_foot, height_in_less_inch);
+
+    cout << "your height is " << height_in_inch << " inches" << endl;
+    cout << "your weight is " << weight_in_pound << " pound" << endl;
+
+    cout << "your BMI is " << myFunction2(weight_in_pound, height_in_inch);
+    
     return 0;
 }
 
-// 怎么硕
-void myFunction1(int inch)
+int myFunction1(int height_in_foot, int height_in_less_inch)
 {
-    std::cout << "your height is " << inch / 12;
-    std::cout << " feet and " << inch % 12 << " inches." << std::endl;
+    return height_in_foot * factor_foot_to_inch \
+        + height_in_less_inch;
+}
+
+double myFunction2(double weight_in_pound, int height_in_inch)
+{
+    double var = 0;
+
+    var = height_in_inch * factor_inch_to_meter;
+    var *= var;
+
+    return weight_in_pound / factor_km_to_pound / var;
 }
